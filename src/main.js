e303f84885dfef7662845c7eb4110901cc7e5f59
Ishizaki Taishi <ishizaki.taishi@donuts.ne.jp>
2017-09-26 12:58:48 +0900

    init

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import Vuex from 'vuex';
+import Vuetify from 'vuetify'
+
+import App from './App.vue'
+import './stylus/main.styl'
+
+Vue.use(Vuex);
+Vue.use(Vuetify);
+
+new Vue({
+    el: '#app',
+    render: h => h(App)
+})
